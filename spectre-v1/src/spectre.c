f2d74cda5efbea8abf2ac6b80d5c7ee07b4e6eba
rob-petenzi <rob.petenzi@gmail.com>
2025-06-26 12:52:16 +0200

    added array2

--- spectre-v1/src/spectre.c
+++ spectre-v1/src/spectre.c
@@ -12,6 +12,7 @@ uint64_t array1_sz = 16;
 uint8_t unused1[64];
 uint8_t array1[16] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
 uint8_t unused2[64];
+uint8_t array2[256 * L1_BLOCK_SZ_BYTES];
 
 char* secretString = "!\"#ThisIsTheBabyBoomerTest";
 
